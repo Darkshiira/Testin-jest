4975264ddf0b47e752c6d14f75c42dc2b73b2247
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-03-31 15:51:59 +0200

    connected db with endpoints login and register

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const server = express();
 server.use(express.json());
 const joi = require('joi');
+const db = require('./database');
 
 const schema = joi.object({
     username: joi.string().min(3).max(30).required(),
@@ -15,16 +16,33 @@ server.post('/register', (req, res) => {
 
     if(error) { return res.status(400).json({message: 'username and password is required'})
     }
-    res.status(201).json({message: 'user created'})
+
+    const {username, password} = req.body;
+
+    db.Names.insert({username, password}).then(() => {
+        res.status(201).json({message: 'user created'})
+    })
 
 })
 
 server.post('/login', (req, res) => {
-    const {error} = schema.validate(req.body);
+    const {error, value} = schema.validate(req.body);
 
     if (error) { return res.status(401).json({message: 'username and password is required'})
     }
-    res.status(200).json({message: 'user logged in'})
+
+    const {username, password} = value;
+
+    db.Names.find().toArray().then((users) => {
+        const user = users.find((user) => user.username === username && user.password === password);
+
+        if (user) {
+            return res.status(200).json({message: 'user logged in'})
+        }
+
+        res.status(403).json({message: 'username or password is incorrect'})
+    })
+    
 })
 
 exports.server = server;
