e440b26e55f2742dde3d78f2a667a3a8eedc7a0f
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-03-31 15:51:21 +0200

    Tried creating a db from mongodb

---
+++ server/database.js
@@ -0,0 +1,8 @@
+const {MongoClient} = require('mongodb');
+const url = 'mongodb+srv://Darkshiira:Boodise87@cluster0.wcjvn29.mongodb.net/?retryWrites=true&w=majority';
+
+const db = {'Names': undefined}
+
+MongoClient.connect(url).then((client) => db.Names = client.db('Test').collection('Names'));
+
+exports.db = db;
