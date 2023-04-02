12ba01c4af26fd3bb846b9932e5d0d095e2aab0c
Metin Ahmad <metin.ahmad@paligo.net>
2023-04-02 16:02:40 +0200

    Added function to connect to database

--- server/database.js
+++ server/database.js
@@ -1,9 +1,23 @@
-const {MongoClient} = require('mongodb');
-const url = 'mongodb+srv://Darkshiira:Boodise87@cluster0.wcjvn29.mongodb.net/?retryWrites=true&w=majority';
+const { MongoClient } = require("mongodb");
+const url =
+  "mongodb+srv://Darkshiira:Boodise87@cluster0.wcjvn29.mongodb.net/?retryWrites=true&w=majority";
 
-const db = {Names: undefined}
+const db = {
+  Names: undefined,
+  connect: async () =>
+    MongoClient.connect(url)
+      .then((client) => {
+        db.Names = client.db("Test").collection("Names");
+        console.log("connected to database");
+      })
+      .catch((error) => console.log(error)),
+};
 
-MongoClient.connect(url).then((client) => {db.Names = client.db('Test').collection('Names')
-console.log('connected to database')});
+// MongoClient.connect(url)
+//   .then((client) => {
+//     db.Names = client.db("Test").collection("Names");
+//     console.log("connected to database");
+//   })
+//   .catch((error) => console.log(error));
 
 exports.db = db;
