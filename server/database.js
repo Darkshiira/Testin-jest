5d9759e594895eca3df8d809d197dd579afc6e2d
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-04-02 13:27:14 +0200

    updated syntax of db

--- server/database.js
+++ server/database.js
@@ -1,7 +1,7 @@
 const {MongoClient} = require('mongodb');
 const url = 'mongodb+srv://Darkshiira:Boodise87@cluster0.wcjvn29.mongodb.net/?retryWrites=true&w=majority';
 
-const db = {'Names': undefined}
+const db = {Names: undefined}
 
 MongoClient.connect(url).then((client) => {db.Names = client.db('Test').collection('Names')});
 
