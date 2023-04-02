2a0a7a4b7cd459c08eff01f66d3c744553088a57
Metin Ahmad <metin.ahmad@paligo.net>
2023-04-02 17:54:17 +0200

    Fixed typos

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const server = express();
 server.use(express.json());
 const joi = require('joi');
-const {db} = require('./database');
+const {db} = require('./MongoDatabase');
 
 const schema = joi.object({
     username: joi.string().min(3).max(30).required(),
@@ -18,7 +18,7 @@ server.post('/register', async (req, res) => {
 
     const {username, password} = req.body;
 
-    const createdUser = await db.Names.insertOne({username: username, password: password});
+    const createdUser = await db.names.insertOne({username: username, password: password});
       if (createdUser){ res.status(201).json({message: 'user created'})}
       else {res.status(500).json({message: 'something went wrong'})}
 })
@@ -31,7 +31,7 @@ server.post('/login', async (req, res) => {
 
     const {username, password} = value;
 
-   const user = await db.Names.find({username: username, password: password});
+   const user = await db.names.find({username: username, password: password});
 
         if (user) {
             return res.status(200).json({message: 'user logged in'})
