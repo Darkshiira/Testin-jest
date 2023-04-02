a053372150388c7c3d66390202afa7b34892295a
Metin Ahmad <metin.ahmad@paligo.net>
2023-04-02 16:03:12 +0200

    Regist now awaits a connection to the database

--- test/server.test.js
+++ test/server.test.js
@@ -1,9 +1,11 @@
+const { db } = require('../server/database');
 const {server} = require('../server/server');
 const request = require('supertest');
 
 describe('testing endpoints', () => {
 
     test('post to /register should return 201 statuscode if username and password is sufficient', async () => {
+        await db.connect();
 
         const reponse = await request(server).post('/register').send({username: 'test', password: 'test'})
         expect(reponse.status).toBe(201)
